e208119102f5851aa9cd47427c91508f1e1f915d
MSF Jarvis <msfjarvis@hmamail.com>
2015-08-26 15:29:06 +0530

    Simulator
    Commented out regions are buggy and/or redundant. Help in debugging is
    appreciated

--- Bank_Acc_Sim.py
+++ Bank_Acc_Sim.py
@@ -1,5 +1,4 @@
 class BankAccount():
-"""A class to simulate a bank account"""
     def __init__(self,name,balance):
         self.balance = balance
         self.name = name
@@ -8,8 +7,8 @@ class BankAccount():
     
     def deposit(self,deposit):
         self.balance += deposit
-        self.statement["Deposited"] = amount
-        print deposit,"depositted to Mr.",self.name,"'s Account"
+##        self.statement["Deposited"] = amount
+        print deposit,"depositted to",self.name,"'s Account"
         print "Current Balance is",self.balance
         
     def withdraw(self,amount):
@@ -18,16 +17,19 @@ class BankAccount():
 
         else :
             self.balance -= amount
-            self.statement["Withdrawen"] = amount
+##            self.statement["Withdrawen"] = amount
             print "After this operation your balance is:",self.balance
-    def del_ver(self):
-        ver = raw_input("Are you sure(Y/N): ")
-        if ver == "Y" :
-            print "Destructor started"
-            
-        elif ver == "N" :
-            print "Why are you playing around???"
-            
-        else :
-            print "You're such a jerk!!!"
-            print "Can't you read a simple instruction ???"
+##    def del_ver(self):
+##        ver = raw_input("Are you sure(Y/N): ")
+##        if ver == "Y" :
+##            print "Destructor started"
+##            
+##        elif ver == "N" :
+##            print "Why are you playing around???"
+##            
+##        else :
+##            print "You're such a jerk!!!"
+##            print "Can't you read a simple instruction ???"
+
+    def __del__(self):
+        print "Your account has been submitted for closure. Thank you for banking with us."
