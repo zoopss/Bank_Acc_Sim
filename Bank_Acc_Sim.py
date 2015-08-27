178322b5441bced48eaae95257577df057b04bac
MSF Jarvis <msfjarvis@gmail.com>
2015-08-27 10:09:37 +0530

    Full Rewrite
    Added loan and statement functionality

--- Bank_Acc_Sim.py
+++ Bank_Acc_Sim.py
@@ -2,13 +2,14 @@ class BankAccount():
     def __init__(self,name,balance):
         self.balance = balance
         self.name = name
-        self.statement = {"Initial Balance",self.balance}
+        self.statement ="Initial Balance :",self.balance,"\n"
+        self.debt = 0
         print "Bank Account created successfully for",self.name,"with balance",self.balance
     
     def deposit(self,deposit):
         self.balance += deposit
-##        self.statement["Deposited"] = amount
-        print deposit,"depositted to",self.name,"'s Account"
+        self.statement += "Deposited :", deposit,"\n"
+        print deposit,"deposited to",self.name,"'s Account"
         print "Current Balance is",self.balance
         
     def withdraw(self,amount):
@@ -17,19 +18,27 @@ class BankAccount():
 
         else :
             self.balance -= amount
-##            self.statement["Withdrawen"] = amount
+            self.statement += "Withdrawn :",amount,"\n"
             print "After this operation your balance is:",self.balance
-##    def del_ver(self):
-##        ver = raw_input("Are you sure(Y/N): ")
-##        if ver == "Y" :
-##            print "Destructor started"
-##            
-##        elif ver == "N" :
-##            print "Why are you playing around???"
-##            
-##        else :
-##            print "You're such a jerk!!!"
-##            print "Can't you read a simple instruction ???"
 
+
+
+    def loan(self,amount):
+        rate = 0
+        if amount <= 50000 :
+            rate = 6
+        elif amount > 50000 & amount < 100000 :
+            rate = 8.5
+        elif amount > 100000 :
+            rate = 11
+
+        self.debt += amount
+        self.statement += "Loan Approved :",amount,"\n"
+        print "Your loan of",amount,"has been approved at a rate of",rate
+
+    def statement(self):
+        self.statement += "Balance :",self.balance,"\n"
+
+        
     def __del__(self):
         print "Your account has been submitted for closure. Thank you for banking with us."
