a66e5d05349a7ceac5dbbb5478abaa9efda936f5
Harsh Shandilya <msfjarvis@gmail.com>
2015-10-30 00:41:36 +0530

    Partially converted program into separate functions.
    Hwhs

--- Bank_Acc_Sim.py
+++ Bank_Acc_Sim.py
@@ -1,72 +1,81 @@
-import random
+﻿import random
 class BankAccount():
-    def __init__(self,name,balance):
-        self.balance = balance
-        self.name = name
-        self.auth = int(raw_input("Enter a 4-Digit PIN for your account : "))
-        
-        self.statement ="Initial Balance :",self.balance
-        self.anum = random.randint(0,100000)
-        self.debt = 0
-        print "Bank Account created successfully for",self.name,"with balance",self.balance,"And account number", self.anum
-    
-    def deposit(self,deposit):
-        self.balance += deposit
-        self.statement += "Deposited :", deposit
-        print deposit,"deposited to",self.name,"'s Account"
-        print "Current Balance is",self.balance
-        
-    def withdraw(self,amount):
-        if amount >= self.balance :
-            print "Sorry, you have insufficient funds"
-            
-        else :
-            pin1 = int(raw_input("Enter your PIN : "))
-            if pin1 == self.auth :
-                self.balance -= amount
-                self.statement += "Withdrawn :",amount
-                print "After this operation your balance is:",self.balance
+    def __init__(self,name,balance):
+        self.balance = balance
+        self.name = name
+        self.auth = int(raw_input("Enter a 4-Digit PIN for your account : "))
+        self.statement ="Initial Balance :",self.balance
+        self.anum = random.randint(0,100000)
+        self.debt = 0
+        print "Bank Account created successfully for",self.name,"with balance",self.balance,"And account number", self.anum
 
-            else :
-                print "Authentication Failed..."
+    
+    def deposit(self,deposit):
+        self.balance += deposit
+        self.statement += "Deposited :", deposit
+        print deposit,"deposited to",self.name,"'s Account"
+        print "Current Balance is",self.balance        
+
+    def withdraw(self,amount):
+        if amount >= self.balance :
+            print "Sorry, you have insufficient funds"         else :
+            pin1 = int(raw_input("Enter your PIN : "))
+            if pin1 == self.auth :
+                self.balance -= amount
+                self.statement += "Withdrawn :",amount
+                print "After this operation your balance is:",self.balance
+
+            else :
+                print "Authentication Failed..."
+
+    def loan(self,amount):
+        rate = 0
+        if amount <= 50000 :
+            rate = 6
+        elif amount > 50000 & amount < 100000 :
+            rate = 8.5
+        elif amount > 100000 :
+
+            rate = 11
+        self.debt += amount
+        self.statement += "Loan Approved :",amount
+        print "Your loan of",amount,"has been approved at a rate of",rate,"% per annum"
+
+    def statement(self):
+        print "" + self.statement
+        print "Balance :",self.balance
+
+    def __del__(self):
+
+        print "Your account has been submitted for closure. Thank you for banking with us."
+
+#----------------Class Definition Ends here----------------
+
+acc_init():
+    init_name=raw_input("Enter your name here : ")
+    init_bal = raw_input (" Enter starting balance : ")
+    BankAccount(init_name,init_balance)       
+
+def acc_dep():
+    dep_amount=float(raw_input("Enter amount to be deposited into the account")
 
 
 
-    def loan(self,amount):
-        rate = 0
-        if amount <= 50000 :
-            rate = 6
-        elif amount > 50000 & amount < 100000 :
-            rate = 8.5
-        elif amount > 100000 :
-            rate = 11
+def main():
+    print 'Welcome to ABC Bank'
 
-        self.debt += amount
-        self.statement += "Loan Approved :",amount
-        print "Your loan of",amount,"has been approved at a rate of",rate,"% per annum"
+    print 'What do you want to do ?'
 
-    def statement(self):
-        print "" + self.statement
-        print "Balance :",self.balance
+    print '1. Open a new account'
+    print '2. Deposit Money into an account'
+    print '3. Withdraw Money from your account'
+    print '4. Take a loan'
+    print '5. Generate a statement'
 
-        
-    def __del__(self):
-        print "Your account has been submitted for closure. Thank you for banking with us."
-        
-        
-        
-        #-------------main-----------------
-        
-print 'Welcome to ABC Bank'
-print 'What do you want to do ?'
-print '1. Open a new account'
-print '2. Deposit Money into an account'
-print '3. Withdraw Money from your account'
-print '4. Take a loan'
-print '5. Generate a statement'
 
-choice = int(raw_input("Enter your choice..."))
-if choice==1 :
-    print 'Enter Your Name',
-    name = raw_input('')
+    choice = int(raw_input("Enter your choice..."))
 
+    if choice==1 :
+        acc_init()
+    elif choice==2 :
+        acc_dep()
