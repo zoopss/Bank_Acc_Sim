6e124ff4aa4fe5a50ab7150152de832ab34946d1
Harsh Shandilya <msfjarvis@gmail.com>
2015-10-30 10:02:09 +0530

    Fixed Typos and added some new functions and conditions
    The program is in for a full rewrite, just that i've gotta re-teach myself

--- Bank_Acc_Sim.py
+++ Bank_Acc_Sim.py
@@ -1,63 +1,63 @@
 ﻿import random
 class BankAccount():
-    def __init__(self,name,balance):
-        self.balance = balance
-        self.name = name
-        self.auth = int(raw_input("Enter a 4-Digit PIN for your account : "))
-        self.statement ="Initial Balance :",self.balance
-        self.anum = random.randint(0,100000)
-        self.debt = 0
-        print "Bank Account created successfully for",self.name,"with balance",self.balance,"And account number", self.anum
+    def __init__(self,name,balance):
+        self.balance = balance
+        self.name = name
+        self.auth = int(raw_input("Enter a 4-Digit PIN for your account : "))
+        self.statement ="Initial Balance :",self.balance
+        self.anum = random.randint(0,100000)
+        self.debt = 0
+        print "Bank Account created successfully for",self.name,"with balance",self.balance,"And account number", self.anum
 
-    
-    def deposit(self,deposit):
-        self.balance += deposit
-        self.statement += "Deposited :", deposit
-        print deposit,"deposited to",self.name,"'s Account"
-        print "Current Balance is",self.balance        
+    def deposit(self,deposit):
+        self.balance += deposit
+        self.statement += "Deposited :", deposit
+        print deposit,"deposited to",self.name,"'s Account"
+        print "Current Balance is",self.balance        
 
-    def withdraw(self,amount):
-        if amount >= self.balance :
-            print "Sorry, you have insufficient funds"         else :
-            pin1 = int(raw_input("Enter your PIN : "))
-            if pin1 == self.auth :
-                self.balance -= amount
-                self.statement += "Withdrawn :",amount
-                print "After this operation your balance is:",self.balance
+    def withdraw(self,amount):
+        if amount >= self.balance :
+            print "Sorry, you have insufficient funds"         else :
+            pin1 = int(raw_input("Enter your PIN : "))
+            if pin1 == self.auth :
+                self.balance -= amount
+                self.statement += "Withdrawn :",amount
+                print "After this operation your balance is:",self.balance
 
-            else :
-                print "Authentication Failed..."
+            else :
+                print "Authentication Failed..."
 
-    def loan(self,amount):
-        rate = 0
-        if amount <= 50000 :
-            rate = 6
-        elif amount > 50000 & amount < 100000 :
-            rate = 8.5
-        elif amount > 100000 :
+    def loan(self,amount):
+        rate = 0
+        if amount <= 50000 :
+            rate = 6
+        elif amount > 50000 & amount < 100000 :
+            rate = 8.5
+        elif amount > 100000 :
+            rate = 11
+        self.debt += amount
+        self.statement += "Loan Approved :",amount
+        print "Your loan of",amount,"has been approved at a rate of",rate,"% per annum"
 
-            rate = 11
-        self.debt += amount
-        self.statement += "Loan Approved :",amount
-        print "Your loan of",amount,"has been approved at a rate of",rate,"% per annum"
+    def statement(self):
+        print "" + self.statement
+        print "Balance :",self.balance
 
-    def statement(self):
-        print "" + self.statement
-        print "Balance :",self.balance
+    def __del__(self):
 
-    def __del__(self):
-
-        print "Your account has been submitted for closure. Thank you for banking with us."
+        print "Your account has been submitted for closure. Thank you for banking with us."
 
 #----------------Class Definition Ends here----------------
 
-acc_init():
+def acc_init():
     init_name=raw_input("Enter your name here : ")
     init_bal = raw_input (" Enter starting balance : ")
-    BankAccount(init_name,init_balance)       
+    init_name = raw_input("Input any name to use as object name : ")
+    init_name = BankAccount(init_name,init_balance)
 
 def acc_dep():
     dep_amount=float(raw_input("Enter amount to be deposited into the account")
+    
 
 
 
@@ -79,3 +79,5 @@ def main():
         acc_init()
     elif choice==2 :
         acc_dep()
+    elif choice==3 :
+        acc_withdraw()
